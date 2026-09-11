33e1230092a6faf13f85669ea2dd58dd6439a5a4
justlofe <lofikpc@gmail.com>
2026-09-11 14:39:15 +0700

    Clean CommandsRegistrarImpl

--- plugin/src/main/java/su/hitori/plugin/module/enable/CommandsRegistrarImpl.java
+++ plugin/src/main/java/su/hitori/plugin/module/enable/CommandsRegistrarImpl.java
@@ -2,27 +2,14 @@ package su.hitori.plugin.module.enable;
 
 import com.mojang.brigadier.tree.LiteralCommandNode;
 import io.papermc.paper.command.brigadier.CommandSourceStack;
-import net.kyori.adventure.key.Key;
-import su.hitori.api.ServerCoreInfo;
 import su.hitori.api.module.enable.CommandsRegistrar;
 
 import java.util.*;
-import java.util.logging.Logger;
 
 public final class CommandsRegistrarImpl implements CommandsRegistrar {
 
-    private final Key moduleKey;
-    private final Logger logger;
-    private final ServerCoreInfo serverCoreInfo;
-
     public final List<LiteralCommandNode<CommandSourceStack>> commands = new ArrayList<>();
     public boolean frozen = false;
-
-    public CommandsRegistrarImpl(Key moduleKey, Logger logger, ServerCoreInfo serverCoreInfo) {
-        this.moduleKey = moduleKey;
-        this.logger = logger;
-        this.serverCoreInfo = serverCoreInfo;
-    }
 
     @SafeVarargs
     @Override
